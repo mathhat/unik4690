696c2cb9f3d1addb07ebd930132d29204c6ee918
curious-discordian <19310012+curious-discordian@users.noreply.github.com>
2018-05-03 11:42:00 +0200

    durpidurp

--- project/source/draw.py
+++ project/source/draw.py
@@ -43,22 +43,24 @@ def draw_humans(npimg, humans, imgcopy=False,):
         reye = tryvar(14)
         nose = tryvar(0)
         neck = tryvar(1)
-            
         
         #head and neck
         if (lear and rear): #head circle if both ears are present
             try:                                             #+ neckline
                 lx = lear[0]
                 rx = rear[0]
-                hx = (lx + rx)/2 
-                dx = (lx - rx) #relation for headsize, distance between ears
-                nx = Centers[1][0]
-                hy =  int((Centers[16][1]+Centers[17][1])/2-dx/2.7)
+                hx = (lx + rx)/2 #Nope
+                taneyes = 0 #Bruker heller tangens her for å få en relativ vinkel.
+                # Plasser etterpå "midpunktet" "over" basert på vinkel. 
+                dx = np.linalg.norm(np.array(lear)-np.array(rear))
+                #^relation for headsize, distance between ears
+                nx = neck[0] 
+                hy =  int((rear[1]+lear[1])/2-dx/2.7) #nope 
                 earringsxl = int((lx+nx)/1.95)
                 earringsxr = int((rx+nx)/2.05)  
-                earringsy = (Centers[0][1]+Centers[1][1])/2 #neck nose mid
+                earringsy = (nose[1]+neck[1])/2 #neck nose mid
                 limblen= int(dx/5.5)
-                cv2.circle(npimg, (hx,hy), int(abs(dx*0.6)), col, thickness=limblen, lineType=8, shift=0)
+                cv2.circle(npimg, (hx,hy), int(abs(dx*0.5)), col, thickness=limblen, lineType=8, shift=0)
                 limblen=dx/4
                 cv2.line(npimg, (lx,Centers[17][1]), (earringsxl,earringsy) , col, limblen)
                 cv2.line(npimg, (rx,Centers[16][1]), (earringsxr,earringsy) , col, limblen)
@@ -68,8 +70,8 @@ def draw_humans(npimg, humans, imgcopy=False,):
         #head pointing left (left eye hidden)
         elif rear:#head circle if right eye is present + faceline
             try:
-                rx = Centers[16][0]          
-                dx = int((Centers[15][0]-Centers[14][0])/4)#dist between eyes
+                rx = rear[0]          
+                dx = int(leye[0]-reye[0])/4#dist between eyes
                 earringsxr = (rx+Centers[1][0])/2
                 earringsy = (Centers[0][1]+Centers[1][1])/2 #neck nose mid
 
