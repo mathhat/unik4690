acc1b634e75d1b8d09ee172c30216eab78971fac
Joseph Knutson <joffern@live.no>
2018-05-22 19:42:35 +0200

    cleaned up in runwebcam

--- project/source/run_webcam.py
+++ project/source/run_webcam.py
@@ -22,12 +22,9 @@ fps_time = 0
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='tf-pose-estimation realtime webcam')
     parser.add_argument('--camera', type=int, default=0)
-    parser.add_argument('--zoom', type=float, default=1)
     parser.add_argument('--back', type=str, default='Images/back.jpg')  #../images/p2.jpg')
     parser.add_argument('--resolution', type=str, default='432x368', help='network input resolution. default=432x368')#def 432 368
     parser.add_argument('--model', type=str, default='mobilenet_thin', help='cmu / mobilenet_thin')
-    parser.add_argument('--show-process', type=bool, default=False,
-                        help='for debug purpose, if enabled, speed for inference is dropped.')
     args = parser.parse_args()
 
     w, h = model_wh(args.resolution)
@@ -35,28 +32,13 @@ if __name__ == '__main__':
     cam = cv2.VideoCapture(args.camera)
     ret_val, image = cam.read()
     back  = common.read_imgfile(args.back,None,None)
-    #back= cv2.cvtColor(back, cv2.COLOR_BGR2GRAY)/255.
-    #cv2.namedWindow('tf-pose-estimation result')
-    #back[:w,:h]
-
+    
     tol1,tol2,tol = read.Tol()
-    #humans = e.inference(image)
     while True:
         ret_val, image = cam.read()
         humans = e.inference(image)
-        #imbw = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)/255.
-        #JOSEPH: 
-        # Hvorfor sender vi inn et fargebilde til denne ? 
-        # spes- siden vi endrer det til sort-hvitt rett etter
-        #jack:
-        # godt spørsmål, jeg tror tegning bare fungerer på fargebilder
-        # tror jeg har tatt feil, men når jeg prøver å sende inn imbw så skjer skumle saker tho
         image2,centers = draw_humans(image.copy(), humans,1)     
-        #image2= cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)/255.
-        
-        #back = np.zeros_like(image2)
         image3 = Laplacian_blend(image/255., back/255.,image2/255.)
-        #image3 = cv2.threshold(image3,0.,255,cv2.THRESH_TOZERO)[1]
         
         cv2.putText(image3,
                     "FPS: %f" % (1.0 / (time.time() - fps_time)),
